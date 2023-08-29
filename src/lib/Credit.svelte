57ed9bf01e47e157bf69ae8814dc1c033ea5fcb2
AnotherPillow <Thepilous_@outlook.com>
2023-08-29 20:45:05 +0100

    Add fancy favicon thanks arcane

--- src/lib/Credit.svelte
+++ src/lib/Credit.svelte
@@ -3,13 +3,23 @@
     <a href="https://www.nexusmods.com/stardewvalley/mods/7416">Custom Menu Background</a>
 </p>
 
+<p class="icon">
+    Favicon by Arcane    
+</p>
+
 <style lang="scss">
     p {
         position: fixed;
         bottom: 0;
 
+        color: white;
+
         width: 100%;
         left: 0;
         text-align: center;
+
+        &.icon {
+            margin: 0 4px;
+        }
     }
 </style>
